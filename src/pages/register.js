6c41f2eb8a89629c630c77850b31ef082ae2af95
Adriana Giraldo <1531nana@gmail.com>
2022-09-27 18:58:52 -0500

    estilos registros mobile

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,15 +4,14 @@ export const Register =
 <video src="./resources/pexels-vanessa-loring-5858145.mp4" autoplay loop muted></video>
 </div>
 <div class= "registerView">
-<h2>Registro</h2>
-<input type="text" id="registerNameUser" placeholder="Nombre">
-<input type="text" id="registerEmailUser" placeholder="Correo electrónico">
-<input type="text" id="registerPassWordUser" placeholder="Contraseña">
+<h2>REGISTRO</h2>
+<input type="text" class="input registerNameUser" id="registerNameUser" placeholder="Nombre">
+<input type="text" class="input registerEmailUser" id="registerEmailUser" placeholder="Correo electrónico">
+<input type="text" class="input registerrPassWordUser" id="registerPassWordUser" placeholder="Contraseña">
 <button id="bottonRegister">REGÍSTRARSE</button>
-</div>
-<div id="google">    
 <button id="bottonGoogle">
 <img id="imgGoogle" src="./resources/google-mas.png" alt="Google">
 </button>
 </div>
+
 `
